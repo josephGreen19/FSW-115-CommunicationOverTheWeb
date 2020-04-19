02e5035edd792d5b0668f60b7fd8dafb68540b60
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2020-04-19 15:27:59 -0400

    firstPostman

--- Week2/Assignment1/index.js
+++ Week2/Assignment1/index.js
@@ -5,40 +5,96 @@ document.body.append(newD)
 var newP = document.createElement("p");
 document.body.append(newP)
 // API Data
-var starWars =  {
-	"name": "Luke Skywalker",
-	"height": "172",
-	"mass": "77",
-	"hair_color": "blond",
-	"skin_color": "fair",
-	"eye_color": "blue",
-	"birth_year": "19BBY",
-	"gender": "male",
-	"homeworld": "https://swapi.co/api/planets/1/",
-	"films": [
-		"https://swapi.co/api/films/2/",
-		"https://swapi.co/api/films/6/",
-		"https://swapi.co/api/films/3/",
-		"https://swapi.co/api/films/1/",
-		"https://swapi.co/api/films/7/"
-	],
-	"species": [
-		"https://swapi.co/api/species/1/"
-	],
-	"vehicles": [
-		"https://swapi.co/api/vehicles/14/",
-		"https://swapi.co/api/vehicles/30/"
-	],
-	"starships": [
-		"https://swapi.co/api/starships/12/",
-		"https://swapi.co/api/starships/22/"
-	],
-	"created": "2014-12-09T13:50:51.644000Z",
-	"edited": "2014-12-20T21:17:56.891000Z",
-	"url": "https://swapi.co/api/people/1/"
-}
+var poki =  {
+	
+		"count": 964,
+		"next": "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20",
+		"previous": null,
+		"results": [
+			{
+				"name": "bulbasaur",
+				"url": "https://pokeapi.co/api/v2/pokemon/1/"
+			},
+			{
+				"name": "ivysaur",
+				"url": "https://pokeapi.co/api/v2/pokemon/2/"
+			},
+			{
+				"name": "venusaur",
+				"url": "https://pokeapi.co/api/v2/pokemon/3/"
+			},
+			{
+				"name": "charmander",
+				"url": "https://pokeapi.co/api/v2/pokemon/4/"
+			},
+			{
+				"name": "charmeleon",
+				"url": "https://pokeapi.co/api/v2/pokemon/5/"
+			},
+			{
+				"name": "charizard",
+				"url": "https://pokeapi.co/api/v2/pokemon/6/"
+			},
+			{
+				"name": "squirtle",
+				"url": "https://pokeapi.co/api/v2/pokemon/7/"
+			},
+			{
+				"name": "wartortle",
+				"url": "https://pokeapi.co/api/v2/pokemon/8/"
+			},
+			{
+				"name": "blastoise",
+				"url": "https://pokeapi.co/api/v2/pokemon/9/"
+			},
+			{
+				"name": "caterpie",
+				"url": "https://pokeapi.co/api/v2/pokemon/10/"
+			},
+			{
+				"name": "metapod",
+				"url": "https://pokeapi.co/api/v2/pokemon/11/"
+			},
+			{
+				"name": "butterfree",
+				"url": "https://pokeapi.co/api/v2/pokemon/12/"
+			},
+			{
+				"name": "weedle",
+				"url": "https://pokeapi.co/api/v2/pokemon/13/"
+			},
+			{
+				"name": "kakuna",
+				"url": "https://pokeapi.co/api/v2/pokemon/14/"
+			},
+			{
+				"name": "beedrill",
+				"url": "https://pokeapi.co/api/v2/pokemon/15/"
+			},
+			{
+				"name": "pidgey",
+				"url": "https://pokeapi.co/api/v2/pokemon/16/"
+			},
+			{
+				"name": "pidgeotto",
+				"url": "https://pokeapi.co/api/v2/pokemon/17/"
+			},
+			{
+				"name": "pidgeot",
+				"url": "https://pokeapi.co/api/v2/pokemon/18/"
+			},
+			{
+				"name": "rattata",
+				"url": "https://pokeapi.co/api/v2/pokemon/19/"
+			},
+			{
+				"name": "raticate",
+				"url": "https://pokeapi.co/api/v2/pokemon/20/"
+			}
+		]
+	}
 //make starWars into a string
-var json = JSON.stringify(starWars)
+var json = JSON.stringify(poki)
 console.log(json)
 
 // Put starWars variable inside newP, make starWars the content that is shown
