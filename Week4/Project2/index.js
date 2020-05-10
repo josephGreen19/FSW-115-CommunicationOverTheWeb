10c992e43ca41db23f14a4818ab791817d25db45
josephGreen19 <58785146+josephGreen19@users.noreply.github.com>
2020-05-09 21:09:49 -0400

    button

--- Week4/Project2/index.js
+++ Week4/Project2/index.js
@@ -1,29 +1,28 @@
-const xhr = new XMLHttpRequest()
-
-xhr.open("GET", "https://rickandmortyapi.com/api/character/", true)
-xhr.send()
-
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4 && xhr.status === 200){
-        const JSONdata = xhr.responseText
-        const data = JSON.parse(JSONdata)
-        showData(data.results)
-    }
-}
-
-function showData(arr){
-    for( let i = 0; i < arr.length; i++){
-        const h1 = document.createElement("h1")
-        h1.textContent = arr[i].name
-        document.body.append(h1)
-    }
-}
-
 function myFunction() {
-  var btn = document.createElement("h2");
-  btn.innerHTML = "heelllppp"
-  document.body.appendChild(btn);
+    fetch("https://rickandmortyapi.com/api/character")
+    .then(res => res.json() )
+    .then(res => {
+        for (let i = 0; i < res.results.length; i++){
+            const h1 = document.createElement('h1')
+            h1.textContent = res.results[i].name
+            document.body.appendChild(h1)
+
+        }
+    })
+    
+
+
+function otherFunction () {
+    fetch("https://swapi.dev/api/people/")
+        .then(res => res.json()  )
+        .then(res => {
+            for (let i = 0; i < res.data.length; i++) {
+                const h1 = document.createElement('h1')
+                h1.textContent = res.data[i].name
+                document.body.appendChild(h1)
+            }
+        })
+}
 }
 
- 
- 
+
---
+++ Week4/Project2/page2.js
@@ -0,0 +1,14 @@
+function otherFunction () {
+    fetch("https://swapi.dev/api/people/")
+        .then(res => res.json()  )
+        .then(res => {
+            for (let i = 0; i < res.results.length; i++) {
+                const h1 = document.createElement('h1')
+                h1.textContent = res.results[i].name
+                document.body.appendChild(h1)
+            }
+        })
+}
+
+
+
